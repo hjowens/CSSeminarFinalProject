5996233e999a2b26f0a6f12e2c72b6f80b9a7c45
HarryJOwens <harrisonjowens123@gmail.com>
2022-05-31 09:11:13 -0400

    Started Buildings

--- Bastion/Assets/Scripts/Building.cs
+++ Bastion/Assets/Scripts/Building.cs
@@ -6,6 +6,9 @@ public class Building : MonoBehaviour
 {
     public int food;
     public int production;
+    public int gold;
+    public int iron;
+    public int stone;
     public int popslots = 1;
     public int productionLeft;
 
